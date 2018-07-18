0461b8743abd8d0c4583e9712602c137fa14435c
void aire() <31969697+aire1@users.noreply.github.com>
2018-07-18 15:12:46 +0300

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
-sudo apt-get update && sudo apt install -y git gawk
+sudo apt install -y git
 
+. /etc/os-release
 PASSWORD=`tr -dc A-Za-z0-9 < /dev/urandom | head -c 15 | xargs`
 CONFIG="{\n\"server\":\"0.0.0.0\",\n\"server_port\":443,\n\"password\":\"$PASSWORD\",\n\"timeout\":30,\n\"method\":\"chacha20-ietf-poly1305\",\n\"fast_open\":true,\n\"reuse_port\": true,\n\"plugin\":\"obfs-server\",\n\"plugin_opts\":\"obfs=tls\",\n\"mode\": \"tcp_and_udp\"\n}"
-VERSION=`gawk '/^Description: Ubuntu [0-9]/ {print $3; exit;}' /usr/share/python-apt/templates/Ubuntu.info`
 IP=`wget -qO- digitalresistance.dog/myIp`
-echo "========================================================== Работа на версиях Ubuntu ниже 16.04 не гарантируется! =========================================================="
 
 setup() {
-echo "========================================================== УСТАНОВКА НАЧАТА =========================================================="
-sudo apt install --no-install-recommends build-essential git autoconf libtool libssl-dev libpcre3-dev libc-ares-dev libev-dev asciidoc xmlto automake -y && sudo git clone https://github.com/shadowsocks/simple-obfs.git && cd simple-obfs && sudo git submodule update --init --recursive && sudo ./autogen.sh && sudo ./configure && sudo make && sudo make install
+echo "Установка simple-obfs..."
+sudo apt-get -y install --no-install-recommends build-essential autoconf libtool libssl-dev libpcre3-dev libev-dev asciidoc xmlto automake
+git clone https://github.com/shadowsocks/simple-obfs.git
+cd simple-obfs
+git submodule update --init --recursive
+./autogen.sh
+./configure && make
+sudo make install
 
 cd /etc/shadowsocks-libev/
-
+	
 sudo rm config.json
 
 sudo echo "$CONFIG" > config.json
@@ -24,24 +29,48 @@ echo "========================================================= УСТАНОВК
 exit 0
 }
 
-new_install() {
+ub_new_install() {
 sudo apt install shadowsocks-libev
 
 setup
 }
 
 
-old_install() {
+ub_old_install() {
 sudo apt-get install software-properties-common -y
-
 sudo add-apt-repository ppa:max-c-lv/shadowsocks-libev -y
-
 sudo apt install -y shadowsocks-libev
-
 setup
 }
 
-case $VERSION in
+deb_new_install() {
+sudo sh -c 'printf "deb http://deb.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list'
+sudo apt update
+sudo apt -t stretch-backports install shadowsocks-libev
+}
+
+deb_old_install() {
+sudo sh -c 'printf "deb http://deb.debian.org/debian jessie-backports main\n" > /etc/apt/sources.list.d/jessie-backports.list'
+sudo sh -c 'printf "deb http://deb.debian.org/debian jessie-backports-sloppy main" >> /etc/apt/sources.list.d/jessie-backports.list'
+sudo apt update
+sudo apt -t jessie-backports-sloppy install shadowsocks-libev
+}
+
+echo "========================================================== УСТАНОВКА НАЧАТА =========================================================="
+
+case $VERSION_ID in
+	8)
+        deb_old_install
+        ;;
+	9)
+        deb_new_install
+        ;;
+	14.04)
+        old_install
+        ;;
+	16.04)
+        old_install
+        ;;
         16.10)
         new_install
         ;;
@@ -54,7 +83,11 @@ case $VERSION in
 	18.04)
         new_install
         ;;
+	18.04)
+        new_install
+        ;;	
 	*)
-	old_install
+	echo "Ваша версия Linux не поддерживается!\nПоддерживается Ubuntu версии 14.04+ и Debian версии 8+"
+	exit 1
 	;;
 esac
