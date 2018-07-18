3547d32bb2d5f4a8d1a514f395b314d2d0af7d7d
void aire() <31969697+aire1@users.noreply.github.com>
2018-07-18 19:18:37 +0300

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -94,9 +94,6 @@ case $VERSION_ID in
 	18.04)
         ub_new_install
         ;;
-	18.04)
-        ub_new_install
-        ;;
 	*)
 	echo "Ваша версия Linux не поддерживается!\nПоддерживается Ubuntu версии 14.04+ и Debian версии 8+"
 	exit 1
